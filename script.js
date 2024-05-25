df889b1605087a0df6fcdbe5361d631f2249bc52
shrutiboop <64666409+shrutiboop@users.noreply.github.com>
2024-05-25 19:26:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function getWeather() {
+    const location = document.getElementById('locationInput').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e18ada8d57931e292f54697fae93392f&units=metric`)
+        
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+                <h2>Weather in ${data.name}</h2>
+                <p>Temperature: ${data.main.temp} °C</p>
+                <p>Weather: ${data.weather[0].description}</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
